06735a019b4c8e432f4a826e8b6791d55d786b54
Mateuzkl <mateus.roberto@educacao.mg.gov.br>
2026-04-03 10:52:34 -0300

    fix: remove walkthrough delay requiring two attempts in protection zones

--- src/player.cpp
+++ src/player.cpp
@@ -788,18 +788,6 @@ bool Player::canWalkthrough(const Creature* creature) const
 		return false;
 	}
 
-	Player* thisPlayer = const_cast<Player*>(this);
-	if ((OTSYS_TIME() - lastWalkthroughAttempt) > 2000) {
-		thisPlayer->setLastWalkthroughAttempt(OTSYS_TIME());
-		return false;
-	}
-
-	if (creature->getPosition() != lastWalkthroughPosition) {
-		thisPlayer->setLastWalkthroughPosition(creature->getPosition());
-		return false;
-	}
-
-	thisPlayer->setLastWalkthroughPosition(creature->getPosition());
 	return true;
 }
 
